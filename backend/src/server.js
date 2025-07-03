9d2f585d0f4336f6c8ab9bcbb54f1b16fbfb3999
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2025-07-03 10:29:15 +0100

    created delete api endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { db } from "./config/db.js";
 import { ENV } from "./config/env.js";
 import { favoritesTable } from "./db/schema.js";
+import { and, eq } from "drizzle-orm";
 
 const app = express();
 const PORT = ENV.PORT;
@@ -14,26 +15,49 @@ app.get("/api/health", (req, res) => {
 
 app.post("/api/favorites", async (req, res) => {
   try {
-    console.log(req.body)
+    console.log(req.body);
     const { userId, recipeId, title, image, cookTime, servings } = req.body;
 
     if (!userId || !recipeId || !title) {
       return res.status(400).json({ error: "Missing required fields" });
     }
-    
-    const newFavorite = await db.insert(favoritesTable).values({
-      userId,
-      recipeId,
-      title,
-      image,
-      cookTime,
-      servings,
-    }).returning();
-    
-    res.status(201).json(newFavorite[0])
+
+    const newFavorite = await db
+      .insert(favoritesTable)
+      .values({
+        userId,
+        recipeId,
+        title,
+        image,
+        cookTime,
+        servings,
+      })
+      .returning();
+
+    res.status(201).json(newFavorite[0]);
   } catch (error) {
-    console.log("Error adding favorite", error)
-    res.status(500).json({ error: "Somthing went wrong" })
+    console.log("Error adding favorite", error);
+    res.status(500).json({ error: "Somthing went wrong" });
+  }
+});
+
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+  try {
+    const { userId, recipeId } = req.params;
+
+    await db
+      .delete(favoritesTable)
+      .where(
+        and(
+          eq(favoritesTable.userId, userId),
+          eq(favoritesTable.recipeId, parseInt(recipeId))
+        )
+      );
+
+      res.status(200).json({ message: "Favorite deleted successfully" });
+  } catch (error) {
+    console.log("Error removing a favorite", error);
+    res.status(500).json({ error: "Somthing went wrong" });
   }
 });
 
