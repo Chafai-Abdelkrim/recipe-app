0dc7b893e6b6abadef2f57bce721779d4aaf769d
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2025-07-09 09:22:47 +0100

    added handlesingIn function

--- mobile/app/(auth)/sign-in.jsx
+++ mobile/app/(auth)/sign-in.jsx
@@ -1,6 +1,64 @@
-import { Text, View } from "react-native";
+import { useSignIn } from "@clerk/clerk-expo";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import { Image } from "expo-image";
+
+import { authStyles } from "../../assets/styles/auth.styles";
+import { COLORS } from "../../constants/colors";
 
 const SignInScreen = () => {
+  const router = useRouter();
+
+  const { signIn, setActive, isLoaded } = useSignIn();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSignIn = async () => {
+    if (!email || !password) {
+      Alert.alert("Error", "Please fill in all fields");
+      return;
+    }
+
+    if (!isLoaded) return;
+
+    setLoading(true);
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: email,
+        password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+      } else {
+        Alert.alert("Error", "Sign in failed. Please try again.");
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err) {
+      Alert.alert("Error", err.errors?.[0]?.message || "Sign in failed");
+      console.error(JSON.stringify(err, null, 2));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  
   return (
     <View>
       <Text>SignInScreen</Text>
