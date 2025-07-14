ee23cb4f7cc91ab977fe26b45429e7679407f695
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2025-07-14 09:25:41 +0100

    added loadData function to homescreen page

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,6 +1,67 @@
-import { Text, View } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+  RefreshControl,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+import { MealAPI } from "../../services/mealAPI";
+import { homeStyles } from "../../assets/styles/home.styles";
+import { Image } from "expo-image";
+import { COLORS } from "../../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import CategoryFilter from "../../components/CategoryFilter";
+import RecipeCard from "../../components/RecipeCard";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 const HomeScreen = () => {
+  const router = useRouter();
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [featuredRecipe, setFeaturedRecipe] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const loadData = async () => {
+    try {
+      setLoading(true);
+
+      const [apiCategories, randomMeals, featuredMeal] = await Promise.all([
+        MealAPI.getCategories(),
+        MealAPI.getRandomMeals(12),
+        MealAPI.getRandomMeal(),
+      ]);
+
+      const transformedCategories = apiCategories.map((cat, index) => ({
+        id: index + 1,
+        name: cat.strCategory,
+        image: cat.strCategoryThumb,
+        description: cat.strCategoryDescription,
+      }));
+
+      setCategories(transformedCategories);
+
+      if (!selectedCategory) setSelectedCategory(transformedCategories[0].name);
+
+      const transformedMeals = randomMeals
+        .map((meal) => MealAPI.transformMealData(meal))
+        .filter((meal) => meal !== null);
+
+      setRecipes(transformedMeals);
+
+      const transformedFeatured = MealAPI.transformMealData(featuredMeal);
+      setFeaturedRecipe(transformedFeatured);
+    } catch (error) {
+      console.log("Error loading the data", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <View>
       <Text>Homescreen</Text>
