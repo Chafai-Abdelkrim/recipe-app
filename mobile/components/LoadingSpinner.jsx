1db5238e47879cfa0f0ed1c3863193efa8de652d
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2025-07-17 10:07:22 +0100

    added LoadingSpinner component

---
+++ mobile/components/LoadingSpinner.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const LoadingSpinner = () => {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  };
+  export default LoadingSpinner;
